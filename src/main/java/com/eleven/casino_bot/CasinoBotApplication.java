5b412d245008af23f3862afeba38b8677210c210
iqpizza6349 <iqpizza6349@gmail.com>
2022-04-16 21:23:06 +0900

    🌱:: Initial Commit

---
+++ src/main/java/com/eleven/casino_bot/CasinoBotApplication.java
@@ -0,0 +1,11 @@
+package com.eleven.casino_bot;
+
+import com.eleven.casino_bot.config.BotConfig;
+
+public class CasinoBotApplication {
+
+    public static void main(String[] args) {
+        System.out.println(BotConfig.getTOKEN());
+    }
+
+}
