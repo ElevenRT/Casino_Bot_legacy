5f2ceb2f04a7f44fc642998d791ab1b860dc9efd
iqpizza6349 <iqpizza6349@gmail.com>
2022-04-17 17:32:40 +0900

    ⚠:: yml 파일 읽어오기 수정

--- src/main/java/com/eleven/casinobot/CasinoBotApplication.java
+++ src/main/java/com/eleven/casinobot/CasinoBotApplication.java
@@ -1,11 +1,19 @@
 package com.eleven.casinobot;
 
 import com.eleven.casinobot.config.BotConfig;
+import com.eleven.casinobot.listener.BotListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
 
 public class CasinoBotApplication {
 
-    public static void main(String[] args) {
-        System.out.println(BotConfig.getTOKEN());
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA jda = JDABuilder.createDefault(BotConfig.getToken())
+                .addEventListeners(new BotListener())
+                .build();
+        jda.awaitReady();
     }
 
 }
--- src/main/java/com/eleven/casinobot/config/BotConfig.java
+++ src/main/java/com/eleven/casinobot/config/BotConfig.java
@@ -15,20 +15,60 @@ import java.util.HashMap;
 public class BotConfig {
 
     private static String TOKEN;
+    private static String PREFIX;
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+    private static String DEV;
 
     private BotConfig() {
 
     }
 
-    public static String getTOKEN() {
+    public static String getToken() {
         if (TOKEN == null) {
-            TOKEN = getToken();
+            TOKEN = getData(DataType.TOKEN);
         }
         return TOKEN;
     }
 
-    private static String getToken() {
-        String token = "";
+    public static String getPrefix() {
+        if (PREFIX == null) {
+            PREFIX = getData(DataType.PREFIX);
+        }
+        return PREFIX;
+    }
+
+    public static String getURL() {
+        if (URL == null) {
+            getDatabase();
+        }
+        return URL;
+    }
+
+    public static String getUsername() {
+        if (USERNAME == null) {
+            getDatabase();
+        }
+        return USERNAME;
+    }
+
+    public static String getPassword() {
+        if (PASSWORD == null) {
+            getDatabase();
+        }
+        return PASSWORD;
+    }
+
+    public static String getDEV() {
+        if (DEV == null) {
+            DEV = getData(DataType.DEVELOPER);
+        }
+        return DEV;
+    }
+
+    private static String getData(DataType dataType) {
+        String data = "";
 
         try {
             URL url = CasinoBotApplication.class
@@ -38,11 +78,46 @@ public class BotConfig {
             HashMap<String, Object> objects = new Yaml()
                     .load(new FileReader(file.getAbsolutePath()));
             String temp = objects.get("bot").toString().split("\\{")[1];
-            token = temp.substring(6, temp.length()-1);
+            data = temp.split("[,]")[dataType.ordinal()]
+                    .trim()
+                    .substring(dataType.toString().length() + 1);
+            if (dataType == DataType.DEVELOPER) {
+                data = data.substring(0, data.length() - 1);
+            }
         } catch (FileNotFoundException | URISyntaxException e) {
             e.printStackTrace();
         }
-        return token;
+        return data;
+    }
+
+    private static void getDatabase() {
+        try {
+            URL url = CasinoBotApplication.class
+                    .getClassLoader()
+                    .getResource("config.yml");
+            File file = Paths.get(url != null ? url.toURI() : new URI("")).toFile();
+            HashMap<String, Object> objects = new Yaml()
+                    .load(new FileReader(file.getAbsolutePath()));
+            String temp = objects.get("database").toString().split("\\{")[1];
+            URL = temp.split("[,]")[0]
+                    .trim()
+                    .substring(4);
+            USERNAME = temp.split("[,]")[1]
+                    .trim()
+                    .substring(9);
+            PASSWORD = temp.split("[,]")[2]
+                    .trim()
+                    .substring(9);
+            PASSWORD = PASSWORD.substring(0, PASSWORD.length() - 1);
+        } catch (FileNotFoundException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private enum DataType {
+        TOKEN,
+        PREFIX,
+        DEVELOPER
     }
 
 }
