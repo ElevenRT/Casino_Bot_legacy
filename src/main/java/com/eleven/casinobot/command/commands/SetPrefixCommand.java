5f728cc989dd1ca1ffb7f08fa109787c3732339f
황지민 <80872558+krapoi@users.noreply.github.com>
2022-04-18 16:33:07 +0900

    ♻:: Prefix 변경 기능 완성 (#4)
    * ♻:🍃 Prefix 변경 기능 완성
    
    * ♻:🍃 Prefix 변경 기능 완성
    
    * ♻:: 띄어쓰기
    
    * ♻:🍃 Prefix 변경 기능 완성
    
    * ♻:🍃 Prefix 변경 기능 완성

---
+++ src/main/java/com/eleven/casinobot/command/commands/SetPrefixCommand.java
@@ -0,0 +1,48 @@
+package com.eleven.casinobot.command.commands;
+
+import com.eleven.casinobot.command.CommandContext;
+import com.eleven.casinobot.command.ICommand;
+import com.eleven.casinobot.config.PrefixConfig;
+import com.eleven.casinobot.database.DataBaseQuery;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.List;
+
+public class SetPrefixCommand implements ICommand {
+
+    DataBaseQuery query = new DataBaseQuery();
+    @Override
+    public void handle(CommandContext ctx) {
+        final TextChannel channel = ctx.getChannel();
+        final List<String> chat = ctx.getStrings();
+        final Member member = ctx.getMember();
+
+        if (!member.hasPermission(Permission.MANAGE_SERVER)) {
+            channel.sendMessage("이 명령어를 사용하려면 관리자 권한이 필요합니다!").queue();
+            return;
+        }
+
+        if (chat.isEmpty()) {
+            channel.sendMessage("Prefix를 지정하지 않았습니다!").queue();
+            return;
+        }
+
+        final String prefix = String.join("",chat);
+        query.update(channel.getGuild().getIdLong(),prefix, "GUILD", "prefix", "guild_id");
+
+    }
+
+    @Override
+    public String getName() {
+        return "setprefix";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Sets the prefix for this server\n" +
+                "Usage: `" + PrefixConfig.PREFIXS + "setprefix <prefix>`";
+    }
+
+}
---
+++ src/main/java/com/eleven/casinobot/database/DataBaseQuery.java
@@ -0,0 +1,20 @@
+package com.eleven.casinobot.database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DataBaseQuery {
+    public void update(long guildId, String string, String Table, String colum, String where) {
+
+        try (final PreparedStatement preparedStatement = BotDatabase.getConnection()
+                .prepareStatement("UPDATE " + Table +
+                        " SET " + colum + " = ? " +
+                        "where " + where + " = ?")) {
+
+            preparedStatement.setString(1, string);
+            preparedStatement.setString(2, String.valueOf(guildId));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
