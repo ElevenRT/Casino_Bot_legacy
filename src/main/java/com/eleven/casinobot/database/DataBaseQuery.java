9ccfc2c20662cb4dc76991cc4ff92b6b52b6f97b
황지민 <80872558+krapoi@users.noreply.github.com>
2022-04-18 14:31:43 +0900

    ♻:: 띄어쓰기

--- src/main/java/com/eleven/casinobot/database/DataBaseQuery.java
+++ src/main/java/com/eleven/casinobot/database/DataBaseQuery.java
@@ -7,9 +7,9 @@ public class DataBaseQuery {
     public void update(long guildId, String string, String Table, String colum, String where) {
 
         try (final PreparedStatement preparedStatement = BotDatabase.getConnection()
-                .prepareStatement("UPDATE " + Table + " " +
-                        " SET " + colum + " " + " = ? " +
-                        "where " + where + " " + " = ?")) {
+                .prepareStatement("UPDATE " + Table +
+                        " SET " + colum + " = ? " +
+                        "where " + where + " = ?")) {
 
             preparedStatement.setString(1,string);
             preparedStatement.setString(2,String.valueOf(guildId));
